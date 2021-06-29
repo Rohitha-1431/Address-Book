f6c6bfeaf3442ef8ff90bc5b4325494c7db07bfe
unknown <rohithasatya314@gmail.com>
2021-06-29 08:08:19 +0530

    MODIFIED:Adding contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbook;
 import java.util.ArrayList;
+import java.util.Scanner;
 public class AddressBook {
 	static ArrayList<ContactInfo> user = new ArrayList<ContactInfo>();
     static int flag = 0;
@@ -10,11 +11,39 @@ public class AddressBook {
         user.add(a1);
         System.out.println(user);
     }
+    public static void chooseOption() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("choose one option from below \n 1:add contact \n 2:edit contact \n 3:delete contact");
+        int option = scan.nextInt();
+        switch (option) {
+            case 1:
+                AddressBook.addContact();
+                System.out.println("If you want to continue then press 1 or press 2 if you want to quit");
+                int num = scan.nextInt();
+                if (num == 1) {
+                    chooseOption();
+                } else {
+                    break;
+                }
+                break;
+//            case 2:
+//                editContact();
+//                break;
+//            case 3:
+//                deleteContact();
+//                break;
+            default:
+                System.out.println("You choose Wrong Option please choose right option");
+                //Main.chooseOption();
+                break;
+        }
+
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Programme");
         //UC1
         AddressBook.addContact();
-
+        //UC2
+        AddressBook.chooseOption();
     }
 }
