8ec2b95ba8d866cdbc102c463104a79c493dab94
unknown <rohithasatya314@gmail.com>
2021-06-29 07:58:58 +0530

    MODIFIED:Create contacts

--- AddressBook.java
+++ AddressBook.java
@@ -1,6 +1,20 @@
 package com.bridgelabz.addressbook;
+import java.util.ArrayList;
 public class AddressBook {
-public static void main(String [] args) {
-	System.out.println("Welcome to Address Book");
-	}
+	static ArrayList<ContactInfo> user = new ArrayList<ContactInfo>();
+    static int flag = 0;
+
+    public static void addContact() {
+        ContactInfo a1 = new ContactInfo();
+        a1.getInput();
+        user.add(a1);
+        System.out.println(user);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Programme");
+        //UC1
+        AddressBook.addContact();
+
+    }
 }
